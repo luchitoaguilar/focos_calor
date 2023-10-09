40df8c71fe34e2fe5af17402534fce9f8384fe9e
Luis Aguilar <luchitoa@hotmail.com>
2023-10-08 23:20:01 -0400

    cambiosv3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,24 +17,7 @@
                 transparent 100%);
     }
 
-    #radius-shape-1 {
-        height: 220px;
-        width: 220px;
-        top: -60px;
-        left: -130px;
-        background: radial-gradient(#44006b, #ad1fff);
-        overflow: hidden;
-    }
-
-    #radius-shape-2 {
-        border-radius: 38% 62% 63% 37% / 70% 33% 67% 30%;
-        bottom: -60px;
-        right: -110px;
-        width: 300px;
-        height: 300px;
-        background: radial-gradient(#44006b, #ad1fff);
-        overflow: hidden;
-    }
+ 
 
     .bg-glass {
         background-color: hsla(0, 0%, 100%, 0.9) !important;
@@ -50,17 +33,16 @@
                     <!-- Section: Design Block -->
                     <section class="background-radial-gradient overflow-hidden">
 
-
                         <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
                             <div class="row gx-lg-5 align-items-center mb-5">
                                 <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
                                     <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
-                                        ECEM <br />
+                                        ECEME <br />
                                         <span style="color: hsl(218, 81%, 75%)">Escuela de Comando y Estado Mayor del
                                             Ejército</span>
                                     </h1>
                                     <p class="mb-4 opacity-70" style="color: hsl(218, 81%, 85%)">
-                                        Sistema de control de Focos de Calor en las Guarniciones del Ejército
+                                        SISTEMA DE MONITOREO DE ALERTA TEMPRANA DE INCENDIOS FORESTALES DEL EJÉRCITO
                                     </p>
                                 </div>
 
