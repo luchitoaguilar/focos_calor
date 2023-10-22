9a3521d6face92b2ad130629fd1ec2a6cded4ac3
Luis Aguilar <luchitoa@hotmail.com>
2023-10-22 10:00:26 -0400

    cambiosv9

--- app/Models/Outlet.php
+++ app/Models/Outlet.php
@@ -76,9 +76,10 @@ class Outlet extends Model
      */
     public function getMapPopupContentAttribute()
     {
+        dd($this->unidad);
         $division = Divisiones::where('id', $this->division)->first();
         $unidad = Unidades::where('dependencia', $this->unidad)->first();
-dd($unidad);
+
         $mapPopupContent = '';
         $mapPopupContent .= '<div class="my-2"><strong>' . __('outlet.name') . ':</strong><br>' . $this->name_link . '</div>';
         $mapPopupContent .= '<div class="my-2"><strong>' . __('outlet.coordinate') . ':</strong><br>' . $this->coordinate . '</div>';
